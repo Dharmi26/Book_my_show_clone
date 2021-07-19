1c4f6c15d9113c7d2b0a45a6c7cab59a5218649e
Dharmi26 <dharmipatel567@gmail.com>
2021-07-19 16:59:31 +0530

    plays page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MovieHOC from "./HOC/Movie.HOC";
 // Pages
 import HomePage from "./Pages/Home.page";
 import MoviePage from "./Pages/Movie.page";
+import Plays from "./Pages/Plays.page";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -17,6 +18,7 @@ function App() {
     <>
         <DefaultHOC path="/" exact component={HomePage} />
         <MovieHOC path="/movie/:id" exact component={MoviePage}/>
+        <DefaultHOC path="/plays" exact component={Plays} />
       </>
     );
   }
