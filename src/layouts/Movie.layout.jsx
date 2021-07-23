40f7658bc3671686b1f6cfda4634544da545846c
Dharmi26 <dharmipatel567@gmail.com>
2021-07-23 16:55:06 +0530

    added movie context

--- src/layouts/Movie.layout.jsx
+++ src/layouts/Movie.layout.jsx
@@ -1,14 +1,24 @@
+import axios from "axios";
 import React, { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 
 // Components
-import MovieNavbar from "../components/Navbar/navbar.components";
+import MovieNavbar from "../components/Navbar/movieNavbar.component";
 
 // context
 import { MovieContext } from "../context/movie.context";
 
 const MovieLayout = (props) => {
-      const { id } = useParams();
+  const { id } = useParams();
+  const { movie, setMovie } = useContext(MovieContext);
+
+  useEffect(() => {
+    const requestMovie = async () => {
+      const getMovieData = await axios.get(`/movie/${id}`);
+      setMovie(getMovieData.data);
+    };
+    requestMovie();
+  }, [id]);
 
   return (
     <>
